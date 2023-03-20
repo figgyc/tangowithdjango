cde2b0bc74b1df23d32729e5a661c1114889ad5b
George Brooke <2835838b@student.gla.ac.uk>
2023-03-20 09:39:15 +0000

    4.2

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-u*j#^z-xh)!3d(51)#r+#_+pz2a201tkz7il-lp^syrx9ws8*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'george2835.pythonanywhere.com' ]
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -122,4 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'

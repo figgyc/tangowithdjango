f53dd2e7972a65c672ef515b662f9dd21ed2d81a
George Brooke <2835838b@student.gla.ac.uk>
2023-03-20 19:56:38 +0000

    chapter 6 exercises

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,8 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict['categories'] = category_list
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):

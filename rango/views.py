30a6d31390c564c59a737e1ae388bdf6096ca2d0
fGeGeorge Brooke <2835838b@student.gla.ac.uk>
2023-03-20 09:14:54 +0000

    chapter 3 exercises

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse('Rango says here is the about page.')

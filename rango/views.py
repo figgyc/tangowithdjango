f2c876513c3723fefc74d00be306393b5c8743e1
George Brooke <2835838b@student.gla.ac.uk>
2023-04-04 11:55:26 +0100

    8.3 exercise: refactor views.py

--- rango/views.py
+++ rango/views.py
@@ -33,7 +33,7 @@ def add_category(request):
         form = CategoryForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -43,7 +43,7 @@ def add_page(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
     if request.method == 'POST':

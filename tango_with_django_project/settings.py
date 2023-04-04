4fe7f79a10a0074be9c746168a2d2163ef5b05c3
George Brooke <2835838b@student.gla.ac.uk>
2023-04-04 12:36:14 +0100

    9.2: password hashers

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -131,3 +131,10 @@ STATIC_URL = '/static/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
